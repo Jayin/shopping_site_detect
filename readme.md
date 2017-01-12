81df9c102a0e80ee3ae30d4bbe930a10657aa252
Jayin Ton <tonjayin@gmail.com>
2017-01-12 11:43:41 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+### 根据购物网站链接返回网站平台英文名
+
+### setup
+
+```shell
+$ npm install --save shopping_site_detect
+```
+
+### Usage
+
+see `test.js`
---
+++ test.js
@@ -0,0 +1,11 @@
+
+const detect = require('./')
+
+
+console.log(detect('https://www.costco.com/iBUYPOWER-C-i14-Gaming-Desktop---7th-Generation-Intel-Core-i7---8GB-NVIDIA-GTX-1080-Graphics.product.100328743.html'))
+
+console.log(detect('http://www.6pm.com/p/guess-gf0160-shiny-rose-gold-gradient-brown/product/8883496/color/690239'))
+
+console.log(detect('http://www.bestbuy.com/site/clp/verizon-iphone-7-trade-in-offer/pcmcat1483461713323.c?id=pcmcat1483461713323'))
+
+console.log(detect('https://www.amazon.com/Apple-iPhone-5S-Certified-Refurbished/dp/B00YD53YQU/ref=sr_1_1?s=wireless&ie=UTF8&qid=1484192213&sr=1-1&keywords=iphone'))
